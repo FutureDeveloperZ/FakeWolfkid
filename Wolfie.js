b8bdf1a525794a803da552da414c4c3752e1a128
shadowolf <shadowolfbusiness@gmail.com>
2018-05-19 10:13:02 +1000

    Fully updating code because it's really wrong.

--- Wolfie.js
+++ Wolfie.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
@@ -7,11 +8,12 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
-    if(message.author.bot) return;
+    if (message.author.bot) return;
+    let prefix = config.prefix;
 
-    if (message.content === '!help') {
+    if (message.content === prefix + 'help') {
       let helpembed = new Discord.RichEmbed()
-      .setTitle('ImpladeBot Commands List')
+      .setTitle('Commands')
       .setDescription('There is available commands for this bot on list!')
       .addField('!help', 'Bot commands list!')
       .addField('!channel', 'Creator Youtube Channel')
@@ -23,30 +25,28 @@ client.on('message', async message => {
 
       return message.channel.send(helpembed);
     };
- 
+
     if (message.content === '!about') {
       let aboutembed = new Discord.RichEmbed()
       .setTitle('Want to Know More About Me?')
       .setDescription('Ok. Im a High Tech Bot That Can do a lot of Stuff Example tell you How many Members are in your Server or in Other Server My Code is a open source you can fine on github i can do more but is time to do other stuff.')
       .setColor('#FFFF00')
-      .addField('GitHub', 'https://github.com/Wolfkid200444/FakeWolfkidBOT')
-      .addField('Report Issues')
-      .setDescription('[Check Out My Discord](http://bit.ly/FutureDevs)')
-
+      .addField('GitHub', 'https://github.com/FutureDeveloperZ/FakeWolfkid')
+      .addField('Report Issues', 'https://github.com/FutureDeveloperZ/FakeWolfkid/issues')
+      .setDescription('Join the Discord', 'http://bit.ly/FutureDevs')
 
       return message.channel.send(aboutembed);
     };
 
-    if (message.content === '!media') {
+    if (message.content === prefix + 'media') {
       let mediaembed = new Discord.RichEmbed()
       .setTitle('This is Wolfie Social Media')
-      .setField('Is Youtube', ' Is Your is www.youtube.com/c/Wolfkid')
-      .setField('Is Twitter', 'Check out WolfKid (@TheRealWolfkid): https://twitter.com/TheRealWolfkid?s=09')
-      .setField('Is github', 'My Github is github.com/Wolfkid200444')
-      .setField('My  FanDiscord Server', 'bit.ly/WolfkidDiscord')
-      .setField('My Website', 'In Progress')
+      .setField('YouTube', 'https://www.youtube.com/c/Wolfkid')
+      .setField('Twitter', 'https://twitter.com/TheRealWolfkid')
+      .setField('GitHub', 'https://github.com/Wolfkid200444')
+      .setField('Fan Discord Server', 'http://bit.ly/WolfkidDiscord')
+      .setField('Website', 'In Progress')
       .setColor('#FFFF00')
-
       return message.channel.send(mediambed);
     };
 
@@ -59,7 +59,7 @@ client.on('message', async message => {
       return message.channel.send(verembed);
     };
 
-    if (message.content === '!changelog') {
+    if (message.content === prefix + 'changelog') {
       let changelogembed = new Discord.RichEmbed()
       .setTitle('Changelog')
       .setDescription('Check out Whats New on The Bot :D')
@@ -67,26 +67,23 @@ client.on('message', async message => {
       .addField('May 14 2018 News Huge Update', 'Added Embeds To Commands And Alot')
       .addField('May 18 2018 News', 'Added Help Command And Few Commands Working again')
       .setColor('#808000')
-      
       return message.channel.send(changelogembed);
     };
-    
-    if (message.content === '!github') {
+
+    if (message.content ===  prefix + 'github') {
       let githubembed = new Discord.RichEmbed()
-      .setTitle('Check Out our Github')
-      .setDescription('Check out our [github](github.com/FutureDeveloperz) to view all of our code or our repository')
+      .setTitle('Check Out our GitHub')
+      .setURL('https://github.com/FutureDeveloperZ/')
+      .setDescription('Check out our GitHub to view all of our code or our repository.')
       .setColor('#000000')
-      
       return message.channel.send(githubembed);
     };
-    
-    if (message.content === '!dcinvite') {
+
+    if (message.content === prefix + 'dcinvite') {
       let dcinviteembed = new Discord.RichEmbed()
       .setTitle('Our Discord Invite')
-      .setDecription('Check out our Discord server to get news of FakeWolfkid and if you have issues with the bot or not working property you can report it on #report or jn our Github')
-      .setField('The Link', 'The link is bit.ly/FutureDevs')
       .setURL('bit.ly/FutureDevs')
-      
+      .setDecription('Check out our Discord server to get news of FakeWolfkid and if you have issues with the bot or not working property you can report it on #report or jn our Github')
       return message.channel.send(dcinviteembed);
     };
 });
