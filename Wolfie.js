f3aad750b2d651f3ed05a3a55867ff5765fb1663
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-25 14:36:43 -0400

    Update Wolfie.js

--- Wolfie.js
+++ Wolfie.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('We Love !', {type: 'WATCHING'});
+    client.user.setActivity('Neko Lewds!?!', {type: 'WATCHING'});
     console.log('BOI I\'m ready!');
 });
 
@@ -80,10 +80,34 @@ client.on('message', async message => {
 
     if (message.content === prefix + 'dcinvite') {
       let dcinviteembed = new Discord.RichEmbed()
-      .setTitle('Our Discord Invite')
+      .setTitle('Click Here to Our Discord Invite')
       .setURL('http://bit.ly/FutureDevs')
       .setDescription('Check out our Discord server to get news of FakeWolfkid and if you have issues with the bot or not working property you can report it on #report or on our GitHub')
       return message.channel.send(dcinviteembed);
+    };
+    
+    if (command === "lewd") {
+
+        message.delete().catch(O_o=>{});
+
+        let {body} = await superagent
+
+        .get(https://nekos.life/api/lewd/neko);
+
+        if (!message.channel.nsfw) return message.reply("This command is prohibited outside of NSFW channels.");
+
+        let hentaiEmbed = new Discord.RichEmbed()
+
+        .setColor("#ff9900")
+
+        .setTitle("Here is your image!")
+
+        .setImage(body.neko)
+
+        .setFooter(Requested by ${message.author.tag});
+
+        message.channel.send(hentaiEmbed);
+
     };
 });
 
