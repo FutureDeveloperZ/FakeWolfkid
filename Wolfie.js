2b78b7accb94d4af86bfda4995fb3d87f9321e89
sadguy <40127132+SadGuyComplete@users.noreply.github.com>
2018-06-24 19:07:31 -0400

    Add files via upload

--- Wolfie.js
+++ Wolfie.js
@@ -17,7 +17,7 @@ client.on('message', async message => {
       .setTitle('List Of Commands | prefix f; | Help List ')
       .setDescription('There is available commands for this bot on list!')
       .addField(':newspaper: INFO', '7 Commands open \n``help info``')
-      .addField(':tada: FUN', '1 Commands open \n``help fun``)
+      .addField(':tada: FUN', '1 Commands open \n``help fun``')
       .setColor('RANDOM')
       .setFooter(`Requested by ${message.author.tag} | Help list`)
       return message.channel.send(helpembed);
