490a101ca43c34a3098f9671ba7085cb891d5fee
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-10-26 19:40:49 -0400

    Add files via upload

--- Wolfie.js
+++ Wolfie.js
@@ -170,58 +170,7 @@ client.on('message', async message => {
    return message.channel.send(pingembed)
   } 
    
-   function sendInfo(message, ip, port){
-    queryfor = "Query for " + ip + " " + port + ""; 
-    site = "https://use.gameapis.net/mcpe/query/extensive/" + ip + ":" + port;
-    status = "Offline";
-    list = null;
-    plugins = null;
-
-    request({url: site, json: true}, (err, response, i) => {
-        if(!err){
-            console.log(i);
-            if(i["status"] == true){
-                status = "Online";
-            }else{
-                sendOffline(message, ip, port);
-                return;
-            }
-            if(i["list"] == null){
-                list = "None";
-            }else{
-                list = i["list"].join();
-            }
-            if(i["plugins"] == null){
-                plugins = "None";
-            }else{
-                if(i["plugins"] == i["software"]){
-                    plugins = i["software"];
-                }else{
-                    plugins = i["plugins"].join();
-                }
-            }
-
-            message.channel.send(
-                queryfor + " (" + i["players"]["online"] +"/" + i["players"]["max"] +")\n
-" + 
-                "Status: " + status + "\n" +
-                "MOTD: " + i["motd"] + "\n" + 
-                "IP: " + ip + "\n" + 
-                "Port: " + port + "\n" +
-                "Protocol: " + i["protocol"] + "\n" +
-                "Version: " + i["version"] + "\n" +
-                "Software: " + i["software"] + "\n" +
-                "Map: " + i["map"] + "\n" +
-                "List: " + list + "\n" +
-                "Plugins: " + plugins + "\n" +
-                "Cached: " + i["cached"] + "
-"
-                );
-        }
-    });
-}
-   
-   
+    
    
    // MOD Commands
    
