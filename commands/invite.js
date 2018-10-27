ce8996eda03bba69bb62e20fb9e02d3051e1f512
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-10-27 08:14:22 -0400

    Add files via upload

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
       let inviteembed = new Discord.RichEmbed()
       .setTitle('Discord Invite')
       .setDescription('PRIVATE BOT UNTIL I RELEASE IT')
-      .addField('Want the code will search it [HERE](https://github.com/FutureDeveloperZ/FakeWolfkid)
+      .addField('Want the code will search it [HERE](https://github.com/FutureDeveloperZ/FakeWolfkid)')
       .setColor('DARK')
       return message.channel.send(inviteembed);
 }
