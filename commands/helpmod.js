0f079354486b1dbec6eccfdf76667b4ce75adc11
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-10-31 11:36:39 -0400

    Update helpmod.js

--- commands/helpmod.js
+++ commands/helpmod.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       let helpmodembed = new Discord.RichEmbed()
       .setTitle('Mod Commands | prefix f; | Mod list')
-      .setDescription('There is available commands for MOD!\n ``kick, ban, purge``')
+      .setDescription('There is available commands for MOD!\n ``kick, ban, mute, purge``')
       .setColor('RANDOM')
       .setFooter(`Requested by ${message.author.tag} | Help mod`)
       return message.channel.send(helpmodembed);
