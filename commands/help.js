6b610cec06cb83be6687e71b5ff983371cc0ea8f
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-10-31 11:36:13 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
       .setTitle('List of Commands | prefix f; | Help List ')
       .setDescription('There is available commands for this bot on list!')
       .addField(':newspaper: INFO', '6 commands available \n``help_info``')
-      .addField(':tools: MODERATION', '4 commands available \n``help_mod``')
+      .addField(':tools: MODERATION', '5 commands available \n``help_mod``')
       .addField(':tada: FUN', '5 commands available \n``help_fun``')
       .addField(':hammer: OWNER', '2 commamds avalible \n``help_owner``')
       .setColor('RANDOM')
