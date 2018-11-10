695aa99abbfa0adee645c3e34253e3e3b51c4de8
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-10 14:24:39 -0400

    :/

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,13 @@ module.exports.run = async (bot, message, args) => {
       .setTitle('List of Commands | prefix f; | Help List ')
       .setDescription('There is available commands for this bot on list!')
       .addField(':newspaper: INFO', 'Information \n``help_info``')
-      .addField(':tools: MODERATION', 'Mod Info \n``help_mod``')
-      .addField(':tada: FUN', 'Fun Info \n``help_fun``')
-      .addField(':camera: IMAGE', 'Img Info \n``help_img``')
-      .addField(':hammer: OWNER', 'Bot Owner Info \n``help_owner``')
+      .addField(':tools: MODERATION', 'Mod Commans \n``help_mod``')
+      .addField(':tada: FUN', 'Fun Commands \n``help_fun``')
+      .addField(':camera: IMAGE', 'Img Commands \n``help_img``')
+      .addField(':smirk: NSFW', 'NSFW Commands \n``help_nsfw``')
+      .addField(':hammer: OWNER', 'Bot Owner Commands \n``help_owner``')
       .setColor('RANDOM')
+      .setTimestamp()
       .setFooter(`Requested by ${message.author.tag} | Help list`, `${message.author.avatarURL}`)
       return message.channel.send(helpembed);
 
