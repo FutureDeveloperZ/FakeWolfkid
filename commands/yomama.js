00d0d4f393d2ab147078de9d162c2ffdb123ebf0
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-10 11:05:40 -0400

    :/

---
+++ commands/yomama.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const fetch = require('snekfetch');
+
+const emoji = ['🤣', '👅', '😱', '😆', '😂'];
+module.exports.run = async (client, message) => {
+    fetch.get('https://api.apithis.net/yomama.php').then(joe => {
+        const joke = new Discord.RichEmbed()
+    .addField(`${emoji[~~(Math.random() * emoji.length)]}`, joe.body);
+        message.channel.send({embed: joke}).catch(e => logger.error(e));
+    })
+  .catch(e => logger.error(e));
+};
+module.exports.help = {
+    name: 'yomama'
+};
