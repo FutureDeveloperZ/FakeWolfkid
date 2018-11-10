897b9068674f22e9cd8b34a9af70c71c8b3911b3
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-10 11:06:01 -0400

    :/

--- commands/helpowner.js
+++ commands/helpowner.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       let helpownerembed = new Discord.RichEmbed()
       .setTitle('Owner Commands | prefix f; | Owner list')
-      .setDescription('There 2 Available Commands')
+      .setDescription('There 3 Available Commands')
       .addField('📕 GENERA', '``leave, shutdown, eval``')
       .setColor('RANDOM')
       .setFooter(`Requested by ${message.author.tag} | Help Owner`)
