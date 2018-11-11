04cc498d376c113213f8b7ecec314ab21ac6a988
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-10 23:23:38 -0400

    :/

--- commands/helpnsfw.js
+++ commands/helpnsfw.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {      
       let embed = new Discord.RichEmbed()
       .setTitle('NSFW Commands | prefix f; | Help NSFW ')
-      .setDescription('There is 3 Available Command!')  
-      .addField(':smirk: GENERAL', '``lewd, porn, ecchi``')
+      .setDescription('There is 4 Available Command!')  
+      .addField(':smirk: GENERAL', '``lewd, porn, ecchi, neko``')
       .setColor('RANDOM')
       .setTimestamp()
       .setFooter(`Requested by ${message.author.tag} | Help list`, `${message.author.avatarURL}`)
