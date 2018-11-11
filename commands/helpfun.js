ec8c24d980471ac11fe6ce544b0eb2fac814f6ec
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-10 23:16:37 -0400

    :/

--- commands/helpfun.js
+++ commands/helpfun.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       let helpfunembed = new Discord.RichEmbed()
       .setTitle('Fun Commands | prefix f; | Fun list')
-      .setDescription('There is 6 Available Commands ')
+      .setDescription('There is 9 Available Commands ')
       .addField('📕 GENERAL', '``avatar, gayrate, 8ball, poll, ascii, coinflip, quiz ``')
       .addField(':joy: JOKES', '``joke, yomama``')
       .setColor('RANDOM')
