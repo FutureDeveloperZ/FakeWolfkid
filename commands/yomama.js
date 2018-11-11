b80be769f6873dd0aede3a47d280fcf5d0d8e7bb
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-10 23:13:47 -0400

    :/

--- commands/yomama.js
+++ commands/yomama.js
@@ -5,7 +5,9 @@ const emoji = ['🤣', '👅', '😱', '😆', '😂'];
 module.exports.run = async (client, message) => {
     fetch.get('https://api.apithis.net/yomama.php').then(joe => {
         const joke = new Discord.RichEmbed()
-    .addField(`${emoji[~~(Math.random() * emoji.length)]}`, joe.body);
+        .addField(`${emoji[~~(Math.random() * emoji.length)]}`, joe.body)
+        .setTimestamp()
+        .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
         message.channel.send({embed: joke}).catch(e => logger.error(e));
     })
   .catch(e => logger.error(e));
