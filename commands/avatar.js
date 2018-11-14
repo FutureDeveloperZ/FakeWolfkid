5431b9b223dddf066e9da1986ae0942e8585bb60
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2018-11-14 08:54:28 -0400

    :/
    Fix?

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-     let user = message.mentions.users.first() || message.author;
+
      let avatarembed = new Discord.RichEmbed()
-     .setTitle(`${message.author.tag} Avatar`)
+     .setTitle('Your Avatar')
      .setColor('RANDOM')
      .setImage(message.author) 
      .setFooter(`Requested by ${message.author.tag}`, `${message.author.avatarURL}`)
